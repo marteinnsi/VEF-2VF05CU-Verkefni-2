7b1efbaadefdc1d3c0308b3ee40abb44575d2bd3
marteinnsi <marteinnsi216@nemi.tskoli.is>
2020-01-28 09:46:57 +0000

    feat: Setja inn kt

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+@app.route("/")
+def route_index():
+    return app.send_static_file("html/index.html")
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
+@app.route("/kennitala/<int:kt>")
+def route_kennitala(kt):
+    if len(str(kt)) == 10:
+        tversumma = 0
+        while kt:
+            tversumma += kt % 10
+            kt //= 10
+        return str(tversumma)
+    else:
+        return "Kennitala á að vera 10 stafir."
+
+if __name__ == "__main__":
     app.run(threaded=True, port=5000)
